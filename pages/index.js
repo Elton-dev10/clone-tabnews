8df13463480d75cc4c7769811907b9b31734251a
José Elton de Araújo Silva <jose.elton@escolar.ifrn.edu.br>
2025-01-31 23:02:26 +0000

    melhorando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Meu mano Sams, TMJ nessa jornada de programacao. Obrigado mano pela parceria e pelo incentivo de seguir na area de desenvolvimento!</h1>
+    return (
+        <h1>
+            Meu mano Sams, TMJ nessa jornada de programação. <br />
+            Obrigado mano pela parceria e pelo incentivo de seguir na área de desenvolvimento! <br />
+            <img src="https://em-content.zobj.net/source/microsoft-teams/363/smiling-face-with-sunglasses_1f60e.png" 
+            alt="Emoji de óculos escuro" 
+            width="100" />
+        </h1>
+    );
 }
 
 export default Home;
