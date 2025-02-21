12b3026b855f2125cb9dd6a499dde31721a58440
José Elton de Araújo Silva <jose.elton@escolar.ifrn.edu.br>
2025-02-21 02:49:37 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto com base no curso.dev do Filipe Deschamps para treinar programação
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 function Home() {
-    return (
-        <h1>
-            Meu mano Sams, TMJ nessa jornada de programação. <br />
-            Obrigado mano pela parceria e pelo incentivo de seguir na área de desenvolvimento! <br />
-            <img src="https://em-content.zobj.net/source/microsoft-teams/363/smiling-face-with-sunglasses_1f60e.png" 
-            alt="Emoji de óculos escuro" 
-            width="100" />
-        </h1>
-    );
+  return (
+    <h1>
+      Meu mano Sams, TMJ nessa jornada de programação. <br />
+      Obrigado mano pela parceria e pelo incentivo de seguir na área de
+      desenvolvimento! <br />
+      <img
+        src="https://em-content.zobj.net/source/microsoft-teams/363/smiling-face-with-sunglasses_1f60e.png"
+        alt="Emoji de óculos escuro"
+        width="100"
+      />
+    </h1>
+  );
 }
 
 export default Home;
